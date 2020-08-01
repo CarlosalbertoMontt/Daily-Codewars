ddbbf1b22e5ef827a82e965ec2305ad790756ebf
unknown <Carlosalberto.montt@gmail.com>
2020-08-01 04:34:02 -0400

    Commit via Git

---
+++ StringRepeat.js
@@ -0,0 +1,8 @@
+// Write a function called repeat_str which repeats the given string src exactly count times.
+
+function repeatStr (n, s) {
+var str="";
+for(var i=0; i < n; i++)
+  str+=s;
+  return str;
+}
