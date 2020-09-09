388bd2d5d2183a9b12c0a19c3c53fccdcab09bde
unknown <Carlosalberto.montt@gmail.com>
2020-09-09 09:07:19 -0400

    Commit via Git

---
+++ SimpleConsecutivePairs.js
@@ -0,0 +1,16 @@
+/* pairs([1,2,5,8,-4,-3,7,6,5]) = 3
+The pairs are selected as follows [(1,2),(5,8),(-4,-3),(7,6),5]
+--the first pair is (1,2) and the numbers in the pair are consecutive; Count = 1
+--the second pair is (5,8) and are not consecutive
+--the third pair is (-4,-3), consecutive. Count = 2
+--the fourth pair is (7,6), also consecutive. Count = 3.
+--the last digit has no pair, so we ignore. */
+
+
+function pairs(arr){
+  var count=0;
+  for (var i=0; i<arr.length; i+=2)
+    if (arr[i]-1==arr[i+1]||arr[i]+1==arr[i+1])
+      count++;
+  return count;
+};
